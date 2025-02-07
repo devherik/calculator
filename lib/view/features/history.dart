33e9fd4d306beac07ac889a1a34fef9ecedfe766
Herik Colares Rezende <dev.herik@gmail.com>
2025-02-07 04:45:23 -0300

    logic: erase all in history and item copy to clipboard

--- lib/view/features/history.dart
+++ lib/view/features/history.dart
@@ -1,5 +1,7 @@
 import 'package:calculator/viewmodel/expression_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key, required this.expressionViewmodel});
@@ -13,6 +15,7 @@ class HistoryPage extends StatelessWidget {
           'Histórico',
           style: Theme.of(context).textTheme.titleSmall,
         ),
+        actions: [eraseButton()],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
@@ -24,10 +27,7 @@ class HistoryPage extends StatelessWidget {
                   itemCount: value.length,
                   itemBuilder: (context, index) => Column(
                     children: [
-                      Text(
-                        value[index].toString(),
-                        style: Theme.of(context).textTheme.labelLarge,
-                      ),
+                      historyValue(value[index].toString(), context),
                       SizedBox(
                         width: MediaQuery.of(context).size.width,
                         child: Divider(
@@ -43,4 +43,18 @@ class HistoryPage extends StatelessWidget {
       ),
     );
   }
+
+  Widget historyValue(String value, BuildContext context) => GestureDetector(
+        onTap: () async => await Clipboard.setData(ClipboardData(text: value)),
+        child: Text(
+          value,
+          style: Theme.of(context).textTheme.labelLarge,
+        ),
+      );
+
+  Widget eraseButton() => Builder(
+        builder: (context) => IconButton(
+            onPressed: () => expressionViewmodel.clearHistory(),
+            icon: const Icon(Iconsax.eraser)),
+      );
 }
